51f44b6e16667924abd3834e8220f7c6543ebde8
abdelhakbarbouche <barbouchea0@gmail.com>
2024-05-28 10:33:46 +0100

    done for home page

--- Frontend/SmartScreen/App.js
+++ Frontend/SmartScreen/App.js
@@ -6,15 +6,18 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Message from "./screens/Message"
 import Onbording from "./screens/Onbording"
 import Launch from "./screens/Launch "
-
+import Home from "./screens/Home"
+import Help from './screens/Help'
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="StartingPage" screenOptions={{ headerShown: false }}>
-         <Stack.Screen name="Launch" component={Launch} /> 
-         <Stack.Screen name="Onbording" component={Onbording} /> 
+         {/* <Stack.Screen name="Launch" component={Launch} />  */}
+         {/* <Stack.Screen name="Onbording" component={Onbording} />  */}
+         {/* <Stack.Screen name="Home" component={Home} />  */}
+         <Stack.Screen name="Help" component={Help} /> 
         {/* <Stack.Screen name="Message" component={Message} />  */}
        </Stack.Navigator>
     </NavigationContainer>
